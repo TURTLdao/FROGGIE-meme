cc65bd07c34158139d25b0c60276f0ad004e8f0e
TurtleDAO <79334084+TURTLdao@users.noreply.github.com>
2023-06-14 14:13:43 +0100

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default function Header() {
-    const FROGGIES_IMG = "https://pbs.twimg.com/profile_images/1654219239706841091/I1O5j0V7_400x400.jpg";
+    const FROGGIES_IMG = "https://github.com/TURTLdao/TURTL-images/blob/main/forggie.png?raw=true";
     return (
         <header className="container-fluid">
             <nav className="container d-flex justify-content-center align-items-center text-dark py-3">
